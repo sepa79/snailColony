993bf2ee879210eca31e9b559c1d059ebc1ba636
sepa79 <37452386+sepa79@users.noreply.github.com>
2025-09-04 09:23:17 +0100

    chore(server): use built protocol package

--- apps/server/jest.config.js
+++ apps/server/jest.config.js
@@ -2,7 +2,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   rootDir: 'src',
-  moduleNameMapper: {
-    '^@snail/protocol$': '<rootDir>/../../../libs/protocol/src',
-  },
 };
