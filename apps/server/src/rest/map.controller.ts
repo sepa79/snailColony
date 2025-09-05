3baa56b827821ff8e569ca3600b94717d8a41b27
sepa79 <37452386+sepa79@users.noreply.github.com>
2025-09-05 15:43:11 +0100

    Serve map without lobby check

--- apps/server/src/rest/map.controller.ts
+++ apps/server/src/rest/map.controller.ts
@@ -1,20 +1,12 @@
-import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { MapService } from '../game/map.service';
-import { RoomService } from '../game/room.service';
 
 @Controller('map')
 export class MapController {
-  constructor(
-    private readonly maps: MapService,
-    private readonly rooms: RoomService,
-  ) {}
+  constructor(private readonly maps: MapService) {}
 
   @Get(':roomId')
   getMap(@Param('roomId') roomId: string) {
-    const room = this.rooms.getRoom(roomId);
-    if (!room) {
-      throw new BadRequestException('Room not found');
-    }
     return this.maps.load(roomId);
   }
 }
